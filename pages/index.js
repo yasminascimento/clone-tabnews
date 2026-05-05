84910e8abecd05d34b1cf2804f53a5b65f7d7c65
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2026-05-05 10:50:31 +0000

    add ´jsconfig.json´ file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import database from "infra/database.js";
+
+async function status (request, response){
+  const result =await database.query(" select 1+1 as sum");
+  console.log(result.rows);
+  response.status(200).json({chave: "são acima da média"});
+}
+/*
 function Home() {
   return <h1>Oi AMOR, EU TE AMO</h1>;
 }
-
-export default Home;
+*/
+export default status;
